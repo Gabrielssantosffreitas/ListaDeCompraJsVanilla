05ad01be6ddae3f312d8fc2c9b6bbb399fc9f5cd
Gabriel santos <Gabrielsantos022freitas@gmail.com>
2024-04-26 14:13:55 -0300

    modal

---
+++ javaScript/modal.js
@@ -0,0 +1,41 @@
+
+console.log("javaScript")
+class Modal {
+    #modal
+    #botao
+    #corpo
+
+    constructor(modal,botao,corpo)
+    {
+
+        this.#modal = modal
+        this.#botao = botao
+        this.#corpo = corpo
+
+    }
+// toda vez que o botao for cliclado o modal aparece
+    escutarBotao() 
+    {
+        this.#botao.addEventListener('click', () => {
+            this.#modal.style.display = 'block'
+            this.#corpo.style.display = "block"
+        })
+    }
+// toda vez que o modal for cliclado o modal some
+    escutarModal()
+    { 
+        this.#modal.addEventListener("click",()=>{ 
+            this.#modal.style.display = 'none'
+            this.#corpo.style.display = "none"
+        })
+    }
+}
+const modalAdicionar = new Modal(document.querySelector("#modalInvisilvelAdcionar"),document.querySelector("#ModalAdcionar"),document.querySelector("#modalCorpoAdicinar")) 
+const modalEditar = new Modal(document.querySelector("#modalInvisilvelEditar"),document.querySelector("#ModalEditar"),document.querySelector("#modalCorpoEditar"))
+
+
+modalAdicionar.escutarBotao()
+modalAdicionar.escutarModal()
+modalEditar.escutarBotao()
+modalEditar.escutarModal()
+
